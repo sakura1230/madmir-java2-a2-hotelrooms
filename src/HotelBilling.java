3e6f53f4bd31c14c197f7e6ba0bb5c20aaa88cf2
clydewbrown <clydewbrown@gmail.com>
2013-08-16 08:02:02 -0400

    Source files for Assignment #2, Hotel Rooms

---
+++ src/HotelBilling.java
@@ -0,0 +1,21 @@
+
+public class HotelBilling {
+
+	public static void main(String[] args) {
+		/*
+		 * In the main method, declare and initialize variables
+		 * for room price, number of rooms booked, and coupon rate.
+		 * 
+		 * Then call each case of the overloaded method 
+		 * calculateBill() to demonstrate the use of overloaded 
+		 * methods
+		 */
+	}
+
+	/*  
+	 * Here you will create four new methods:
+	 * -- Three cases of the overloaded method calculateBill()
+	 * -- A method called twoDecimalRounding() which will
+	 * 		be called by each case of the calculateBill() method.
+	 */
+}
